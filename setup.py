e28e3372baf0a05340649643baec87b33339ba67
devilking15292 <fantasydreamerx@gmail.com>
2018-02-07 23:50:39 +0530

    setup re-structure

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+from __future__ import absolute_import
+
+import os
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 setup(name='IMDB',
-      version='0.1',
+      version='0.1 beta',
       description='API for IMDB',
       url='https://github.com/devilking15292/IMDB_Api_python.git',
       author='devilking15292',
       author_email='p.hari15292@gmail.com',
       license='MIT',
       packages=['IMDB'],
+	  install_requires=install_requires,
       zip_safe=False)
